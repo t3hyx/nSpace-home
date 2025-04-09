c23fe2ff8b6b5d55c707169872183f6b061a1e9b
Thibault - "Aldrin <77836986+t3hyx@users.noreply.github.com>
2025-04-09 17:18:21 +0200

    feat: adding new colors to IColors type

---
+++ src/utils/colorsHelper.ts
@@ -0,0 +1,16 @@
+import type { IColors } from '../types/types'
+
+export const colors: IColors = {
+  pureWhite: 0xFFFFFF,
+  pureBlack: 0x000000,
+  sunOrange: 0xFFAA00,
+  graphiteGrey: 0x404040,
+  mercuryBlue: 0x3498DB,
+  venusOrange: 0xE67E22,
+  earthGreen: 0x2ECC71,
+  marsRed: 0xE74C3C,
+  jupiterYellow: 0xF1C40F,
+  saturnPurple: 0x9B59B6,
+  uranusTeal: 0x1ABC9C,
+  neptuneBlue: 0x34495E,
+}
